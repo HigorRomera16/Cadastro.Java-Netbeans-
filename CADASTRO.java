52afd4d7e522e62ebbe017e706187edd8087619c
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-04-04 10:52:07 -0300

    Add files via upload

---
+++ CADASTRO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class CADASTRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          
+        //Identificação do Estudante
+        String EstudanteNome = "Higor Romera";
+        int EstudanteID = 17;
+        int EstudanteIdade = 16;
+        float EstudanteFee = 95.00f;
+        String EstudanteSala = "1DSSEDUC";
+        
+        
+        // Variáveis Print
+        System.out.println("Nome do Estudante: " + EstudanteNome);
+        System.out.println("Identificacao do Estudante: " + EstudanteID);
+        System.out.println("Idade do Estudante: " + EstudanteIdade);
+        System.out.println("Nota do Estudante: " + EstudanteFee);
+        System.out.println("Sala do Estudante: " + EstudanteSala);
+    }
+    
+}
