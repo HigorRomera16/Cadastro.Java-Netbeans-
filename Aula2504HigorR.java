5bd205e2d4fb63af901e54f593adfaef99432bb9
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-04-25 09:35:23 -0300

    Add files via upload

---
+++ Aula2504HigorR.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula.pkg2504.higor.r;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula2504HigorR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int crepetição;
+        for(crepetição=1;crepetição <= 10;crepetição++){
+            System.out.println("teste");
+            
+        }
+        int repet=1;
+        
+        while(repet <= 10)
+        {
+            System.out.println("teste2");
+            repet++;
+        }
+        
+        int teste=1;
+        do{
+        System.out.println("teste3");
+        teste++;
+        }while (teste <= 10);
+        
+        
+        }
+    }
+    
+
